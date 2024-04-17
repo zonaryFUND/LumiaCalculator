d991a8b7db5d23ab2ed9cafcbccc792dbb324529
zonaryfund <zonaryfund@gmail.com>
2024-04-18 03:36:45 +0900

    yuki skills

---
+++ src/components/subjects/yuki/e.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import Constants from "./constants.json";
+import Damage from "../damage";
+import { ValuesProps } from "../values";
+import { SubjectSkillProps } from "../props";
+
+const e: React.FC<SubjectSkillProps> = props => (
+    <>
+        雪が指定した方向に突進し、最初に対峙した敵に<Damage skill="E" constants={Constants.E.damage} {...props} />のスキルダメージを与え
+        {Constants.E.distance}m移動します。攻撃された対象は{Constants.E.basic_attack_disable}秒間基本攻撃が封鎖され、ダメージを受けた対象がいる場合は打ち落としのクールダウンが{Constants.E.cooldown_reduction}秒減少します。
+    </>
+);
+
+export default e;
+
+export const values: ValuesProps = {
+    additionalInfo: <>このスキルは壁を越えられません。</>,
+    parameters: [
+        {title: "ダメージ量", values: Constants.E.damage.base},
+        {title: "クールダウン", values: Constants.E.cooldown}
+    ]
+}
---
+++ src/components/subjects/yuki/q.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import Constants from "./constants.json";
+import Damage from "../damage";
+import { ValuesProps } from "../values";
+import { SubjectSkillProps } from "../props";
+
+const q: React.FC<SubjectSkillProps> = props => (
+    <>
+        雪の次の基本攻撃が<Damage skill="Q" constants={Constants.Q.damage} {...props} />の基本攻撃ダメージを与えて
+        {Constants.Q.slow.duration}秒間敵の移動速度を{Constants.Q.slow.effect}％減少させます。<br />
+        <br />
+        端正な服装のボタンがある場合、{Constants.Q.stun}秒間気絶させます。
+    </>
+);
+
+export default q;
+
+export const values: ValuesProps = {
+    additionalInfo: <>使用中、基本攻撃の射程距離が少し増加します。</>,
+    parameters: [
+        {title: "ダメージ量", values: Constants.Q.damage.base},
+        {title: "消費", values: Constants.Q.sp_cost}
+    ]
+}
