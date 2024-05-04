f4fe3eb885c2bef7a5fea620635f6e29df6f60eb
zonaryFUND <zonaryfund@gmail.com>
2024-05-05 04:52:45 +0900

    daniel damages

---
+++ src/components/subjects/daniel/damage-table.ts
@@ -0,0 +1,25 @@
+import { WeaponTypeID } from "@app/entity/equipment";
+import { DamageTable, SkillDamageProps } from "../damage-table";
+import Constants from "./constants.json";
+
+const table: DamageTable = {
+    basicAttack: [
+        "standard",
+        {label: "E後追加ダメージ", skill: "E", damage: Constants.E.damage}
+    ],
+    skill: [
+        [
+            {label: "Q", skill: "Q", damage: Constants.Q.damage},
+            {label: "Q中心", skill: "Q", damage: Constants.Q.center_damage},
+        ],
+        [{label: "W爆発基礎値", skill: "W", damage: Constants.W.damage}],
+        [
+            {label: "R潜入中", skill: "R", damage: Constants.R.damage},
+            {label: `R潜入中全ヒット(${Constants.R.damage_count})`, skill: "R", damage: Constants.R.damage, multiplier: Constants.R.damage_count * 100},
+            {label: "R最終最小", skill: "R", damage: Constants.R.finish_damage},
+            {label: "R最終最大", skill: "R", damage: Constants.R.finish_damage, multiplier: Constants.R.finish_multiplier_max}
+        ]
+    ]   
+}
+
+export default table;
