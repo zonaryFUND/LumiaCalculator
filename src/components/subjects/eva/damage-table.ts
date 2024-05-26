4f70200c0713935f4efd4af8990e753c79809033
zonaryFUND <zonaryfund@gmail.com>
2024-05-27 02:48:00 +0900

    eva damage table

--- src/components/subjects/eva/damage-table.ts
+++ src/components/subjects/eva/damage-table.ts
@@ -1,4 +1,4 @@
-import { DamageTable } from "../damage-table";
+import { DamageTable, DamageTableGenerator } from "../damage-table";
 import Constants from "./constants.json";
 
 const rSet = {
@@ -6,27 +6,27 @@ const rSet = {
     amp: Constants.R.damage.amp * Constants.R.max_stack + Constants.R.stack_damage.amp
 }
 
-const table: DamageTable = {
+const table: DamageTableGenerator = props => ({
     basicAttack: [
         "standard",
-        {label: "T追加ダメージ", skill: "T", value: Constants.T.damage}
+        {label: props.intl.formatMessage({id: "subject.eva.passive-additional"}), skill: "T", value: Constants.T.damage}
     ],
     skill: [
         [
-            {label: "Q通過", skill: "Q", value: Constants.Q.first_damage},
-            {label: "Q爆発", skill: "Q", value: Constants.Q.second_damage},
+            {label: props.intl.formatMessage({id: "subject.eva.q-pass"}), skill: "Q", value: Constants.Q.first_damage},
+            {label: props.intl.formatMessage({id: "subject.eva.q-blast"}), skill: "Q", value: Constants.Q.second_damage},
         ],
         [
-            {label: "W発生", skill: "W", value: Constants.W.first_damage},
-            {label: "W爆発", skill: "W", value: Constants.W.second_damage},
+            {label: props.intl.formatMessage({id: "subject.eva.w-first"}), skill: "W", value: Constants.W.first_damage},
+            {label: props.intl.formatMessage({id: "subject.eva.w-blast"}), skill: "W", value: Constants.W.second_damage},
         ],
-        [{label: "E効果中追加ダメージ", skill: "E", value: Constants.E.damage}],
+        [{label: props.intl.formatMessage({id: "subject.eva.e-additional"}), skill: "E", value: Constants.E.damage}],
         [   
-            {label: "R基本", skill: "R", value: Constants.R.damage},
-            {label: `R${Constants.R.max_stack}スタック時追加ダメージ`, skill: "R", value: Constants.R.stack_damage},
-            {label: `R1セット${Constants.R.max_stack}ティック/${Constants.R.max_stack * Constants.R.tick}秒`, skill: "R", value: rSet}
+            {label: props.intl.formatMessage({id: "subject.eva.r-base"}), skill: "R", value: Constants.R.damage},
+            {label: props.intl.formatMessage({id: "subject.eva.r-additional"}, {value: Constants.R.max_stack}), skill: "R", value: Constants.R.stack_damage},
+            {label: props.intl.formatMessage({id: "subject.eva.r-set"}, {value: Constants.R.max_stack, set: Constants.R.max_stack * Constants.R.tick}), skill: "R", value: rSet}
         ]
     ]
-}
+})
 
 export default table;
