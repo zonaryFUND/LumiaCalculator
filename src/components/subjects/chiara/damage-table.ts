5dc59c138f84d9c7784c1ca1d6881215f51e782d
zonaryFUND <zonaryfund@gmail.com>
2024-05-27 03:11:00 +0900

    chiara damage table / 1.22 fix

--- src/components/subjects/chiara/damage-table.ts
+++ src/components/subjects/chiara/damage-table.ts
@@ -1,4 +1,4 @@
-import { DamageTable, SkillValueProps } from "../damage-table";
+import { DamageTable, DamageTableGenerator, SkillValueProps } from "../damage-table";
 import Constants from "./constants.json";
 
 const qMax = {
@@ -6,33 +6,33 @@ const qMax = {
     amp: Constants.Q.damage.amp + Constants.Q.additional_damage.amp * 4
 }
 
-const table: DamageTable = {
+const table: DamageTableGenerator = props => ({
     basicAttack: ["standard"],
     skill: [
         [
-            {label: "Q1本ヒット", skill: "Q", value: Constants.Q.damage},
-            {label: "Q2本目以降ヒット追加ダメージ", skill: "Q", value: Constants.Q.additional_damage},
-            {label: "Q最大ダメージ", skill: "Q", value: qMax},
-            {label: "Q1本ヒット時回復", skill: "Q", value: Constants.Q.heal, type: "heal"},
-            {label: "Q最大ヒット(5)時回復", skill: "Q", value: Constants.Q.heal, type: "heal", multiplier: [{basic: 500}]}
+            {label: props.intl.formatMessage({id: "subject.chiara.q-1hit"}), skill: "Q", value: Constants.Q.damage},
+            {label: props.intl.formatMessage({id: "subject.chiara.q-2hit"}), skill: "Q", value: Constants.Q.additional_damage},
+            {label: props.intl.formatMessage({id: "subject.chiara.q-max"}), skill: "Q", value: qMax},
+            {label: props.intl.formatMessage({id: "subject.chiara.q-1heal"}), skill: "Q", value: Constants.Q.heal, type: "heal"},
+            {label: props.intl.formatMessage({id: "subject.chiara.q-max-heal"}, {value: 5}), skill: "Q", value: Constants.Q.heal, type: "heal", multiplier: [{basic: 500}]}
         ],
         [
-            {label: "Wシールド最小値", skill: "W", value: Constants.W.shield, type: "shield"},
-            {label: `Wシールド最大値(体力${Constants.W.max_shield_hp}％以下)`, skill: "W", value: Constants.W.shield, type: "shield", multiplier: [{basic: 100 + Constants.W.max_shield}]},
-            {label: "W爆発ダメージ", skill: "W", value: Constants.W.damage}
+            {label: props.intl.formatMessage({id: "subject.chiara.w-min"}), skill: "W", value: Constants.W.shield, type: "shield"},
+            {label: props.intl.formatMessage({id: "subject.chiara.w-max"}, {value: Constants.W.max_shield_hp}), skill: "W", value: Constants.W.shield, type: "shield", multiplier: [{basic: 100 + Constants.W.max_shield}]},
+            {label: props.intl.formatMessage({id: "subject.chiara.w-damage"}), skill: "W", value: Constants.W.damage}
         ],
         [
-            {label: "E命中", skill: "E", value: Constants.E.damage},
-            {label: "E発動", skill: "E", value: Constants.E.second_damage}
+            {label: props.intl.formatMessage({id: "subject.chiara.e-hit"}), skill: "E", value: Constants.E.damage},
+            {label: props.intl.formatMessage({id: "subject.chiara.e-launch"}), skill: "E", value: Constants.E.second_damage}
         ],
         ([
-            {label: "R周囲持続ダメージ/秒", skill: "R", value: Constants.R.damage},
-            {label: "R周囲人数あたり体力回復/秒", skill: "R", value: Constants.R.heal, type: "heal"}
+            {label: props.intl.formatMessage({id: "subject.chiara.r-dot"}), skill: "R", value: Constants.R.damage},
+            {label: props.intl.formatMessage({id: "subject.chiara.r-hot"}), skill: "R", value: Constants.R.heal, type: "heal"}
         ] as SkillValueProps[])
         .concat([...Array(Constants.T.max_stack + 1)].map((_, i) => (
-            {label: `R審判固定ダメージ(烙印スタック${i})`, skill: "R", value: Constants.R.finish_damage, type: "true", multiplier: i == 0 ? undefined : [{basic: 100 + i * Constants.R.additional_damage_per_stack}]} as SkillValueProps)
+            {label: props.intl.formatMessage({id: "subject.chiara.r-execute"}, {value: i}), skill: "R", value: Constants.R.finish_damage, type: "true", multiplier: i == 0 ? undefined : [{basic: 100 + i * Constants.R.additional_damage_per_stack}]} as SkillValueProps)
         ))
     ]
-}
+})
 
 export default table;
