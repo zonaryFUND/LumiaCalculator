cae488db19a40994b0b01503a762f8fe9ab8ff50
zonaryFUND <zonaryfund@gmail.com>
2024-05-26 23:40:45 +0900

    adela damage table locale

--- src/components/subjects/adela/damage-table.ts
+++ src/components/subjects/adela/damage-table.ts
@@ -1,29 +1,29 @@
-import { DamageTable } from "../damage-table";
+import { DamageTable, DamageTableGenerator } from "../damage-table";
 import Constants from "./constants.json";
 
-const table: DamageTable = {
+const table: DamageTableGenerator = props => ({
     basicAttack: ["standard"],
     skill: [
         [
-            {label: "Qポーン", skill: "Q", value: Constants.Q.pawn_damage},
-            {label: "Qクイーン", skill: "Q", value: Constants.Q.queen_damage},
-            {label: "Qポーン誘爆", skill: "Q", value: Constants.Q.pawn_damage, multiplier: [{basic: Constants.W.pawn_queen.damage}]},
-            {label: "Qクイーン誘爆", skill: "Q", value: Constants.Q.queen_damage, multiplier: [{basic: Constants.W.pawn_queen.damage}]}
+            {label: props.intl.formatMessage({id: "subject.adela.q-pawn"}), skill: "Q", value: Constants.Q.pawn_damage},
+            {label: props.intl.formatMessage({id: "subject.adela.q-queen"}), skill: "Q", value: Constants.Q.queen_damage},
+            {label: props.intl.formatMessage({id: "subject.adela.q-pawn-reactivation"}), skill: "Q", value: Constants.Q.pawn_damage, multiplier: [{basic: Constants.W.pawn_queen.damage}]},
+            {label: props.intl.formatMessage({id: "subject.adela.q-queen-reactivation"}), skill: "Q", value: Constants.Q.queen_damage, multiplier: [{basic: Constants.W.pawn_queen.damage}]}
         ],
         [
             {label: "W", skill: "W", value: Constants.W.damage},
-            {label: "W(2ヒット)", skill: "W", value: Constants.W.damage, multiplier: [{basic: 200}]},
-            {label: "W誘爆", skill: "W", value: Constants.W.damage, multiplier: [{basic: Constants.W.pawn_queen.damage}] },
+            {label: props.intl.formatMessage({id: "subject.adela.w-2hit"}), skill: "W", value: Constants.W.damage, multiplier: [{basic: 200}]},
+            {label: props.intl.formatMessage({id: "subject.adela.w-reactivation"}), skill: "W", value: Constants.W.damage, multiplier: [{basic: Constants.W.pawn_queen.damage}] },
         ],
         [
             {label: "E", skill: "E", value: Constants.E.damage},
-            {label: "E誘爆", skill: "E", value: Constants.E.damage, multiplier: [{basic: Constants.W.pawn_queen.damage}]},
+            {label: props.intl.formatMessage({id: "subject.adela.e-reactivation"}), skill: "E", value: Constants.E.damage, multiplier: [{basic: Constants.W.pawn_queen.damage}]},
         ],
         [
             {label: "R", skill: "R", value: Constants.R.damage},
-            {label: "R追加ダメージ(駒1つあたり)", skill: "R", value: Constants.R.per_piece, type: "true"}
+            {label: props.intl.formatMessage({id: "subject.adela.r-damage-per-piece"}), skill: "R", value: Constants.R.per_piece, type: "true"}
         ]
     ]
-}
+})
 
 export default table;
