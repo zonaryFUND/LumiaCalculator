da2b277a7272aada0000e96ae9bba8a0501a43e9
zonaryFUND <zonaryfund@gmail.com>
2024-05-27 00:31:55 +0900

    irem damage table locale

--- src/components/subjects/irem/damage-table.ts
+++ src/components/subjects/irem/damage-table.ts
@@ -1,29 +1,29 @@
-import { DamageTable } from "../damage-table";
+import { DamageTable, DamageTableGenerator } from "../damage-table";
 import Constants from "./constants.json";
 
-const table: DamageTable = {
+const table: DamageTableGenerator = props => ({
     basicAttack: [
         "standard",
-        {label: "イレムR追加ダメージ", skill: "R", value: Constants.IremR.damage},
-        {label: "ネコQ突進追加ダメージ", skill: "Q", value: Constants.CatQ.damage},
+        {label: props.intl.formatMessage({id: "subject.irem.irem-r-aa-additional"}), skill: "R", value: Constants.IremR.damage},
+        {label: props.intl.formatMessage({id: "subject.irem.cat-q-aa-rush"}), skill: "Q", value: Constants.CatQ.damage},
     ],
     skill: [
         [
-            {label: "イレムQ(0バウンド)", skill: "Q", value: Constants.IremQ.damage},
-            {label: "イレムQ(1バウンド)", skill: "Q", value: Constants.IremQ.damage, multiplier: [{basic: (1 + Constants.IremQ.ratio) * 100}]},
-            {label: "イレムQ(2バウンド)", skill: "Q", value: Constants.IremQ.damage, multiplier: [{basic: (1 + Constants.IremQ.ratio * 2) * 100}]},
-            {label: "イレムQ(3バウンド)", skill: "Q", value: Constants.IremQ.damage, multiplier: [{basic: (1 + Constants.IremQ.ratio * 3) * 100}]},
+            {label: props.intl.formatMessage({id: "subject.irem.irem-q-bounce"}, {value: 0}), skill: "Q", value: Constants.IremQ.damage},
+            {label: props.intl.formatMessage({id: "subject.irem.irem-q-bounce"}, {value: 1}), skill: "Q", value: Constants.IremQ.damage, multiplier: [{basic: (1 + Constants.IremQ.ratio) * 100}]},
+            {label: props.intl.formatMessage({id: "subject.irem.irem-q-bounce"}, {value: 2}), skill: "Q", value: Constants.IremQ.damage, multiplier: [{basic: (1 + Constants.IremQ.ratio * 2) * 100}]},
+            {label: props.intl.formatMessage({id: "subject.irem.irem-q-bounce"}, {value: 3}), skill: "Q", value: Constants.IremQ.damage, multiplier: [{basic: (1 + Constants.IremQ.ratio * 3) * 100}]},
         ],
-        [{label: "イレムW", skill: "W", value: Constants.IremW.damage}],
+        [{label: props.intl.formatMessage({id: "subject.irem.irem-w"}), skill: "W", value: Constants.IremW.damage}],
         [
-            {label: "ネコQパンチ", skill: "Q", value: Constants.CatQ.rush_damage},
-            {label: `ネコQパンチ最大ヒット(${Constants.CatQ.rush.amount})`, skill: "Q", value: Constants.CatQ.rush_damage, multiplier: [{basic: Constants.CatQ.rush.amount * 100}]}
+            {label: props.intl.formatMessage({id: "subject.irem.cat-q-punch"}), skill: "Q", value: Constants.CatQ.rush_damage},
+            {label: props.intl.formatMessage({id: "subject.irem.cat-q-punch-max-hit"}, {value: Constants.CatQ.rush.amount}), skill: "Q", value: Constants.CatQ.rush_damage, multiplier: [{basic: Constants.CatQ.rush.amount * 100}]}
         ],
-        [{label: "ネコW", skill: "W", value: Constants.CatW.damage}],
-        [{label: "ネコE", skill: "E", value: Constants.CatE.damage}],
-        [{label: "ネコRシールド", skill: "R", value: Constants.CatR.shield, type: "shield"}]
+        [{label: props.intl.formatMessage({id: "subject.irem.cat-w"}), skill: "W", value: Constants.CatW.damage}],
+        [{label: props.intl.formatMessage({id: "subject.irem.cat-e"}), skill: "E", value: Constants.CatE.damage}],
+        [{label: props.intl.formatMessage({id: "subject.irem.cat-r-shield"}), skill: "R", value: Constants.CatR.shield, type: "shield"}]
     ]    
-}
+})
 
 
 export default table;
