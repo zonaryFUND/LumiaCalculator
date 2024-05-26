528e5586e4f5355a6db5b80caea557a444d8b487
zonaryFUND <zonaryfund@gmail.com>
2024-05-26 23:45:39 +0900

    adriana damage-table locale / 1.22 fix

--- src/components/subjects/adriana/damage-table.ts
+++ src/components/subjects/adriana/damage-table.ts
@@ -1,25 +1,25 @@
-import { DamageTable } from "../damage-table";
+import { DamageTable, DamageTableGenerator } from "../damage-table";
 import Constants from "./constants.json";
 
 const maxQ = Constants.Q.duration / Constants.Q.tick + 1;
 
-const table: DamageTable = {
+const table: DamageTableGenerator = props => ({
     basicAttack: ["standard"],
     skill: [
         [
             {label: "Q", skill: "Q", value: Constants.Q.damage},
-            {label: `Q最大ヒット(${maxQ})`, skill: "Q", value: Constants.Q.damage, multiplier: [{basic: maxQ * 100}]}
+            {label: props.intl.formatMessage({id: "subject.adriana.q-max-hit"}, {value: maxQ}), skill: "Q", value: Constants.Q.damage, multiplier: [{basic: maxQ * 100}]}
         ],
         [
-            {label: "E火炎地帯ダメージ/0.5秒", skill: "E", value: Constants.E.damage}
+            {label: props.intl.formatMessage({id: "subject.adriana.e-fire-damage"}, {value: Constants.E.tick}), skill: "E", value: Constants.E.damage}
         ],
         [
             {label: "R", skill: "R", value: Constants.R.damage}
         ],
         [
-            {label: "T持続ダメージ総量", skill: "T", value: Constants.T.damage}
+            {label: props.intl.formatMessage({id: "subject.adriana.t-dot-sum"}, {value: maxQ}), skill: "T", value: Constants.T.damage}
         ]
     ]
-}
+})
 
 export default table;
