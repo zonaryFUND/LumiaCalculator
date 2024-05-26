f5a7e77a872ce4b9c2c389da966e2d01277f2c4c
zonaryFUND <zonaryfund@gmail.com>
2024-05-27 02:53:59 +0900

    katja damage table / 1.22 rework

--- src/components/subjects/katja/damage-table.ts
+++ src/components/subjects/katja/damage-table.ts
@@ -1,23 +1,23 @@
-import { DamageTable } from "../damage-table";
+import { DamageTable, DamageTableGenerator } from "../damage-table";
 import Constants from "./constants.json";
 
-const table: DamageTable = {
+const table: DamageTableGenerator = props => ({
     basicAttack: [
         "standard",
-        {label: "T追加ダメージ", skill: "T", value: Constants.T.damage}
+        {label: props.intl.formatMessage({id: "subject.katja.passive-additional"}), skill: "T", value: Constants.T.damage}
     ],
     skill: [
         [
-            {label: "Q(最小)", skill: "Q", value: Constants.Q.min_damage},
-            {label: "Q(最大)", skill: "Q", value: Constants.Q.max_damage}
+            {label: props.intl.formatMessage({id: "subject.katja.q-min"}), skill: "Q", value: Constants.Q.min_damage},
+            {label: props.intl.formatMessage({id: "subject.katja.q-max"}), skill: "Q", value: Constants.Q.max_damage}
         ],
         [{label: "E", skill: "E", value: Constants.E.damage}],
         [
-            {label: "R(1発目)", skill: "R", value: Constants.R.first_damage},
-            {label: "R(2発目)", skill: "R", value: Constants.R.second_damage},
-            {label: "R(3発目)", skill: "R", value: Constants.R.third_damage}
+            {label: props.intl.formatMessage({id: "subject.katja.r"}, {value: 1}), skill: "R", value: Constants.R.first_damage},
+            {label: props.intl.formatMessage({id: "subject.katja.r"}, {value: 2}), skill: "R", value: Constants.R.second_damage},
+            {label: props.intl.formatMessage({id: "subject.katja.r"}, {value: 3}), skill: "R", value: Constants.R.third_damage}
         ]
     ]
-}
+})
 
 export default table;
--- src/components/subjects/katja/q.tsx
+++ src/components/subjects/katja/q.tsx
@@ -10,7 +10,8 @@ const q: React.FC<SubjectSkillProps> = props => {
             カティアが弾丸を発射して初めて的中した敵に
             <Value skill="Q" ratio={Constants.Q.min_damage} /> ~ <Value skill="Q" ratio={Constants.Q.max_damage} />
             のスキルダメージを与えます。<br />
-            弾丸が飛んで行った距離に比例してダメージ量が増加します。
+            弾丸が飛んで行った距離に比例してダメージ量が増加します。敵に命中した場合には攻撃速度が
+            {Constants.Q.attack_speed.duration}秒間{Constants.Q.attack_speed.effect[props.skillLevel]}%増加します。
         </>
     );
 }
@@ -21,6 +22,7 @@ export const values: ValuesProps = {
     parameters: [
         {title: "最小ダメージ量", values: Constants.Q.min_damage.base},
         {title: "最大ダメージ量", values: Constants.Q.max_damage.base},
+        {title: "攻撃速度増加量(%)", values: Constants.Q.attack_speed.effect},
         {title: "クールダウン", values: Constants.Q.cooldown},
         {title: "消費", values: Constants.Q.sp_cost}
     ]
