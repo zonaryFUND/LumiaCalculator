1efd2f508ffb0ca68bed927d72fe8e3a3e42a6a6
zonaryFUND <zonaryfund@gmail.com>
2024-05-27 02:36:42 +0900

    emma damage table locale

--- src/components/subjects/emma/damage-table.ts
+++ src/components/subjects/emma/damage-table.ts
@@ -1,25 +1,25 @@
-import { DamageTable } from "../damage-table";
+import { DamageTable, DamageTableGenerator } from "../damage-table";
 import Constants from "./constants.json";
 
-const table: DamageTable = {
+const table: DamageTableGenerator = props => ({
     basicAttack: [
         "standard",
-        {label: "T追加ダメージ", skill: "T", value: Constants.T.damage}
+        {label: props.intl.formatMessage({id: "subject.emma.passive-additional"}), skill: "T", value: Constants.T.damage}
     ],
     skill: [
         [
             {label: "Q", skill: "Q", value: Constants.Q.damage},
-            {label: "Q2ヒット", skill: "Q", value: Constants.Q.damage, multiplier: [{basic: 200}]},
+            {label: props.intl.formatMessage({id: "subject.emma.q-2hit"}), skill: "Q", value: Constants.Q.damage, multiplier: [{basic: 200}]},
         ],
         [{label: "W", skill: "W", value: Constants.W.damage}],
         [{label: "E", skill: "E", value: Constants.E.damage}],
         [
-            {label: "鳩R", skill: "R", value: Constants.R.Q.damage},
-            {label: "帽子R", skill: "R", value: Constants.R.W.damage},
-            {label: "兎R", skill: "R", value: Constants.R.E.damage}
+            {label: props.intl.formatMessage({id: "subject.emma.r-pigeon"}), skill: "R", value: Constants.R.Q.damage},
+            {label: props.intl.formatMessage({id: "subject.emma.r-hat"}), skill: "R", value: Constants.R.W.damage},
+            {label: props.intl.formatMessage({id: "subject.emma.r-rabbit"}), skill: "R", value: Constants.R.E.damage}
         ],
-        [{label: "Tシールド", skill: "T", value: Constants.T.shield, type: "shield"}]
+        [{label: props.intl.formatMessage({id: "subject.emma.passive-shield"}), skill: "T", value: Constants.T.shield, type: "shield"}]
     ]
-}
+})
 
 export default table;
