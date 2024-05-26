1a185b8dcf1aa18d16ec01e397a6609c0efb882b
zonaryFUND <zonaryfund@gmail.com>
2024-05-26 23:57:34 +0900

    arda damage table locale

--- src/components/subjects/arda/damage-table.ts
+++ src/components/subjects/arda/damage-table.ts
@@ -1,26 +1,24 @@
-import { DamageTable } from "../damage-table";
+import { DamageTable, DamageTableGenerator } from "../damage-table";
 import Constants from "./constants.json";
 
-const table: DamageTable = {
-    basicAttack: [
-        "standard"
-    ],
+const table: DamageTableGenerator = props => ({
+    basicAttack: ["standard"],
     skill: [
         [{label: "Q", skill: "Q", value: Constants.Q.damage}],
         [
-            {label: "W出現時", skill: "W", value: Constants.W.damage},
-            {label: "W消滅時", skill: "W", value: Constants.W.vanish_damage},
+            {label: props.intl.formatMessage({id: "subject.arda.w-appear"}), skill: "W", value: Constants.W.damage},
+            {label: props.intl.formatMessage({id: "subject.arda.w-disappear"}), skill: "W", value: Constants.W.vanish_damage},
         ],
         [{label: "E", skill: "E", value: Constants.E.damage}],
         [
-            {label: "RQ文字", skill: "R", value: Constants.R.Q.damage},
-            {label: "RW障壁", skill: "R", value: Constants.R.W.damage},
-            {label: "RW障壁全ヒット(4)", skill: "R", value: Constants.R.W.damage, multiplier: [{basic: 400}]},
-            {label: "RW内部全ヒット(4)", skill: "R", value: Constants.W.damage, multiplier: [{basic: 400}]}
+            {label: props.intl.formatMessage({id: "subject.arda.rq-character"}), skill: "R", value: Constants.R.Q.damage},
+            {label: props.intl.formatMessage({id: "subject.arda.rw-wall"}), skill: "R", value: Constants.R.W.damage},
+            {label: props.intl.formatMessage({id: "subject.arda.rw-wall-max-hit"}, {value: 4}), skill: "R", value: Constants.R.W.damage, multiplier: [{basic: 400}]},
+            {label: props.intl.formatMessage({id: "subject.arda.rw-inner-max-hit"}, {value: 4}), skill: "R", value: Constants.W.damage, multiplier: [{basic: 400}]}
         ],
-        [{label: "T回復", skill: "T", value: Constants.T.heal, type: "heal"}]
+        [{label: props.intl.formatMessage({id: "subject.arda.passive-heal"}), skill: "T", value: Constants.T.heal, type: "heal"}]
     ]
-}
+})
 
 
 export default table;
