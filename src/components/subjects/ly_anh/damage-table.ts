b89d8543fe330cf0b9e0e28ae24ebc7d1fabcc1a
zonaryFUND <zonaryfund@gmail.com>
2024-05-27 00:25:20 +0900

    ly anh damage table locale

--- src/components/subjects/ly_anh/damage-table.ts
+++ src/components/subjects/ly_anh/damage-table.ts
@@ -1,30 +1,29 @@
-import { DamageTable } from "../damage-table";
+import { DamageTable, DamageTableGenerator } from "../damage-table";
 import Constants from "./constants.json";
 
-const table: DamageTable = {
+const table: DamageTableGenerator = props => ({
     basicAttack: [
-        {label: "人間基本攻撃", skill: "T", value: Constants.LyAnhT.human_basic_attack, type: "basic"},
-        {label: "憑依基本攻撃", skill: "T", value: Constants.LyAnhT.possessed_basic_attack, type: "basic"},
-        {label: "悪霊基本攻撃", skill: "T", value: Constants.LyAnhT.ghost_basic_attack},
-        {label: "憑依/悪霊追加固定ダメージ", skill: "T", value: Constants.LyAnhT.additional_damage, type: "true"},
-        {label: "憑依追加ダメージ比例回復", skill: "T", value: Constants.LyAnhT.additional_damage, multiplier: [{basic: Constants.LyAnhT.possesed_heal}], type: "heal"},
-        {label: "悪霊追加ダメージ比例回復", skill: "T", value: Constants.LyAnhT.additional_damage, multiplier: [{basic: Constants.LyAnhT.ghost_heal}], type: "heal"}
+        {label: props.intl.formatMessage({id: "subject.lyanh.human-aa"}), skill: "T", value: Constants.LyAnhT.human_basic_attack, type: "basic"},
+        {label: props.intl.formatMessage({id: "subject.lyanh.possessed-aa"}), skill: "T", value: Constants.LyAnhT.possessed_basic_attack, type: "basic"},
+        {label: props.intl.formatMessage({id: "subject.lyanh.ghost-aa"}), skill: "T", value: Constants.LyAnhT.ghost_basic_attack},
+        {label: props.intl.formatMessage({id: "subject.lyanh.additional-true-damage"}), skill: "T", value: Constants.LyAnhT.additional_damage, type: "true"},
+        {label: props.intl.formatMessage({id: "subject.lyanh.possessed-aa-heal"}), skill: "T", value: Constants.LyAnhT.additional_damage, multiplier: [{basic: Constants.LyAnhT.possesed_heal}], type: "heal"},
+        {label: props.intl.formatMessage({id: "subject.lyanh.ghost-aa-heal"}), skill: "T", value: Constants.LyAnhT.additional_damage, multiplier: [{basic: Constants.LyAnhT.ghost_heal}], type: "heal"}
     ],
     skill: [
-        [{label: "人間Q", skill: "Q", value: Constants.LyAnhQ.damage}],
-        [{label: "人間W", skill: "W", value: Constants.LyAnhW.damage}],
-        [{label: "人間E", skill: "E", value: Constants.LyAnhE.damage}],
-        [{label: "憑依Q", skill: "Q", value: Constants.GhostQ.damage}],
-        [{label: "憑依W", skill: "W", value: Constants.GhostW.damage}],
+        [{label: props.intl.formatMessage({id: "subject.lyanh.humanq"}), skill: "Q", value: Constants.LyAnhQ.damage}],
+        [{label: props.intl.formatMessage({id: "subject.lyanh.humanw"}), skill: "W", value: Constants.LyAnhW.damage}],
+        [{label: props.intl.formatMessage({id: "subject.lyanh.humane"}), skill: "E", value: Constants.LyAnhE.damage}],
+        [{label: props.intl.formatMessage({id: "subject.lyanh.ghostq"}), skill: "Q", value: Constants.GhostQ.damage}],
+        [{label: props.intl.formatMessage({id: "subject.lyanh.ghostw"}), skill: "W", value: Constants.GhostW.damage}],
         [
-            {label: "憑依E振り下ろし", skill: "E", value: Constants.GhostE.first_damage},
-            {label: "憑依E引き寄せ", skill: "E", value: Constants.GhostE.second_damage}
+            {label: props.intl.formatMessage({id: "subject.lyanh.ghoste-swing"}), skill: "E", value: Constants.GhostE.first_damage},
+            {label: props.intl.formatMessage({id: "subject.lyanh.ghoste-pull"}), skill: "E", value: Constants.GhostE.second_damage}
         ],
         [
-            {label: "R飛び出し", skill: "R", value: Constants.LyAnhR.damage}
+            {label: props.intl.formatMessage({id: "subject.lyanh.r-jump-out"}), skill: "R", value: Constants.LyAnhR.damage}
         ]
     ]    
-}
-
+})
 
 export default table;
