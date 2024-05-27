38b27c2cc925ed84d24d7506f71e7f2e6082778b
zonaryFUND <zonaryfund@gmail.com>
2024-05-27 21:59:52 +0900

    sissela damage table locale

--- src/components/subjects/sissela/damage-table.ts
+++ src/components/subjects/sissela/damage-table.ts
@@ -1,23 +1,23 @@
-import { DamageTable, SkillValueProps } from "../damage-table";
+import { DamageTable, DamageTableGenerator, SkillValueProps } from "../damage-table";
 import Constants from "./constants.json";
 
-const table: DamageTable = {
+const table: DamageTableGenerator = props => ({
     basicAttack: [
         "standard",
-        {label: "T追加ダメージ", skill: "T", value: Constants.T.damage}
+        {label: props.intl.formatMessage({id: "subject.sissela.passive-additional"}), skill: "T", value: Constants.T.damage}
     ],
     skill: [
         [
-            {label: "Q通過", skill: "Q", value: Constants.Q.first_damage},
-            {label: "Q着弾", skill: "Q", value: Constants.Q.second_damage}
+            {label: props.intl.formatMessage({id: "subject.sissela.q-pass"}), skill: "Q", value: Constants.Q.first_damage},
+            {label: props.intl.formatMessage({id: "subject.sissela.q-blast"}), skill: "Q", value: Constants.Q.second_damage}
         ],
         [{label: "W", skill: "W", value: Constants.W.damage}],
         [
-            {label: "Eシールド", skill: "E", value: Constants.E.shield, type: "shield"},
-            {label: "Eダメージ", skill: "E", value: Constants.E.damage},
+            {label: props.intl.formatMessage({id: "subject.sissela.e-shield"}), skill: "E", value: Constants.E.shield, type: "shield"},
+            {label: props.intl.formatMessage({id: "subject.sissela.e-damage"}), skill: "E", value: Constants.E.damage},
         ],
         [{label: "R", skill: "R", value: {...Constants.R.damage, lostHP: Constants.R.lost_hp_conversion}}]
     ]   
-}
+})
 
 export default table;
