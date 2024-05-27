f515c44fdd48a0ecb4d1fa240c371081c4f5eb79
zonaryFUND <zonaryfund@gmail.com>
2024-05-27 21:52:51 +0900

    xiukai damage table locale

--- src/components/subjects/xiukai/damage-table.ts
+++ src/components/subjects/xiukai/damage-table.ts
@@ -1,20 +1,20 @@
-import { DamageTable, SkillValueProps } from "../damage-table";
+import { DamageTableGenerator } from "../damage-table";
 import Constants from "./constants.json";
 
-const table: DamageTable = {
+const table: DamageTableGenerator = props => ({
     basicAttack: ["standard"],
     skill: [
         [{label: "Q", skill: "Q", value: Constants.Q.damage}],
-        [{label: "W回復", skill: "W", value: Constants.W.heal, type: "heal"}],
+        [{label: props.intl.formatMessage({id: "subject.xiukai.w-heal"}), skill: "W", value: Constants.W.heal, type: "heal"}],
         [
             {label: "E1", skill: "E", value: Constants.E.first_damage},
             {label: "E2", skill: "E", value: Constants.E.second_damage}
         ],
         [
-            {label: "R1ヒット", skill: "R", value: Constants.R.damage},
-            {label: `R全ヒット(${Constants.R.count})`, skill: "R", value: Constants.R.damage, multiplier: [{basic: Constants.R.count * 100}]}
+            {label: props.intl.formatMessage({id: "subject.xiukai.r-1hit"}), skill: "R", value: Constants.R.damage},
+            {label: props.intl.formatMessage({id: "subject.xiukai.r-max-hit"}, {value: Constants.R.count}), skill: "R", value: Constants.R.damage, multiplier: [{basic: Constants.R.count * 100}]}
         ]
     ]
-}
+})
 
 export default table;
