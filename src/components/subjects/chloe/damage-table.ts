e58b69386960b661810a519f1f57ec4e1d9b6cb5
zonaryFUND <zonaryfund@gmail.com>
2024-05-27 21:12:12 +0900

    chloe damage table locale / 1.22 fix

--- src/components/subjects/chloe/damage-table.ts
+++ src/components/subjects/chloe/damage-table.ts
@@ -1,29 +1,29 @@
-import { DamageTable, SkillValueProps } from "../damage-table";
+import { DamageTable, DamageTableGenerator, SkillValueProps } from "../damage-table";
 import Constants from "./constants.json";
 
-const table: DamageTable = {
+const table: DamageTableGenerator = props => ({
     basicAttack: [
         "standard",
-        {label: "ニナ基本攻撃", skill: "T", value: {attack: 100}, type: "summoned"}
+        {label: props.intl.formatMessage({id: "subject.chloe.nina-aa"}), skill: "T", value: {attack: 100}, type: "summoned"}
     ],
     skill: [
         [{label: "Q", skill: "Q", value: Constants.Q.damage}],
         [
-            {label: "W糸回転固定ダメージ/秒", skill: "W", value: Constants.W.damage, type: "true"},
-            {label: `W糸回転固定ダメージ最大ヒット(${Constants.W.duration})秒`, skill: "W", value: Constants.W.damage, type: "true", multiplier: [{basic: Constants.W.duration * 100}]},
-            {label: "W刃ダメージ", skill: "W", value: Constants.W.drop_damage},
-            {label: "Wニナ落下ダメージ", skill: "W", value: Constants.W.nina_damage}
+            {label: props.intl.formatMessage({id: "subject.chloe.w-string-damage-per-sec"}), skill: "W", value: Constants.W.damage, type: "true"},
+            {label: props.intl.formatMessage({id: "subject.chloe.w-string-damage-max"}, {value: Constants.W.duration}), skill: "W", value: Constants.W.damage, type: "true", multiplier: [{basic: Constants.W.duration * 100}]},
+            {label: props.intl.formatMessage({id: "subject.chloe.w-blade"}), skill: "W", value: Constants.W.drop_damage},
+            {label: props.intl.formatMessage({id: "subject.chloe.w-nina"}), skill: "W", value: Constants.W.nina_damage}
         ],
         [
             {label: "E1", skill: "E", value: Constants.E.first_damage},
             {label: "E2", skill: "E", value: Constants.E.second_damage}
         ],
         [
-            {label: "R連結ライン最小固定ダメージ/秒", skill: "R", value: Constants.R.damage, type: "true"},
-            {label: "R連結ライン最大固定ダメージ/秒", skill: "R", value: Constants.R.damage, multiplier: [{basic: Constants.R.damage_max_multipler * 100}], type: "true"},
+            {label: props.intl.formatMessage({id: "subject.chloe.r-min"}), skill: "R", value: Constants.R.damage, type: "true"},
+            {label: props.intl.formatMessage({id: "subject.chloe.r-max"}), skill: "R", value: Constants.R.damage, multiplier: [{basic: Constants.R.damage_max_multipler * 100}], type: "true"},
         ],
-        [{label: "Tニナ突き追加ダメージ", skill: "T", value: Constants.T.damage}]
+        [{label: props.intl.formatMessage({id: "subject.chloe.passive-nina"}), skill: "T", value: Constants.T.damage}]
     ]
-}
+})
 
 export default table;
