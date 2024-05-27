d824315e951d9f8cf79c3c11c5f544f74cc0b7d0
zonaryFUND <zonaryfund@gmail.com>
2024-05-27 23:23:07 +0900

    sua damage table locale

--- src/components/subjects/sua/damage-table.ts
+++ src/components/subjects/sua/damage-table.ts
@@ -1,42 +1,42 @@
-import { DamageTable, SkillValueProps } from "../damage-table";
+import { DamageTable, DamageTableGenerator, SkillValueProps } from "../damage-table";
 import Constants from "./constants.json";
 
-const table: DamageTable = {
+const table: DamageTableGenerator = props => ({
     basicAttack: [
         "standard",
-        {label: "T追加ダメージ(対象)", skill: "T", value: Constants.T.damage},
-        {label: "T追加ダメージ(周囲)", skill: "T", value: Constants.T.aoe_damage}
+        {label: props.intl.formatMessage({id: "subject.sua.passive-additional"}), skill: "T", value: Constants.T.damage},
+        {label: props.intl.formatMessage({id: "subject.sua.passive-additional-aoe"}), skill: "T", value: Constants.T.aoe_damage}
     ],
     skill: [
         [
             {label: "Q", skill: "Q", value: Constants.Q.damage},
-            {label: "Q(栞あり)", skill: "Q", value: Constants.Q.bookmark_damage}
+            {label: props.intl.formatMessage({id: "subject.sua.q-bookmark"}), skill: "Q", value: Constants.Q.bookmark_damage}
         ],
         [
-            {label: "Wシールド", skill: "W", value: Constants.W.shield, type: "shield"},
-            {label: "Wダメージ", skill: "W", value: Constants.W.damage}
+            {label: props.intl.formatMessage({id: "subject.sua.w-shield"}), skill: "W", value: Constants.W.shield, type: "shield"},
+            {label: props.intl.formatMessage({id: "subject.sua.w-damage"}), skill: "W", value: Constants.W.damage}
         ],
         [
             {label: "E", skill: "E", value: Constants.E.damage},
-            {label: "E(栞あり)", skill: "E", value: Constants.E.bookmark_damage},
-            {label: "E回復最小値", skill: "E", value: Constants.E.heal, type: "heal"},
-            {label: "E回復最大値", skill: "E", value: Constants.E.heal, type: "heal", multiplier: [{basic: Constants.E.heal_max_multiplier * 100}]},
+            {label: props.intl.formatMessage({id: "subject.sua.e-bookmark"}), skill: "E", value: Constants.E.bookmark_damage},
+            {label: props.intl.formatMessage({id: "subject.sua.e-heal-min"}), skill: "E", value: Constants.E.heal, type: "heal"},
+            {label: props.intl.formatMessage({id: "subject.sua.e-heal-max"}), skill: "E", value: Constants.E.heal, type: "heal", multiplier: [{basic: Constants.E.heal_max_multiplier * 100}]},
         ],
         [
             {label: "RQ", skill: "R", value: Constants.RQ.damage},
-            {label: "RQ(栞あり)", skill: "R", value: Constants.RQ.bookmark_damage}
+            {label: props.intl.formatMessage({id: "subject.sua.rq-bookmark"}), skill: "R", value: Constants.RQ.bookmark_damage}
         ],
         [
-            {label: "RWシールド", skill: "R", value: Constants.RW.shield, type: "shield"},
-            {label: "RWダメージ", skill: "R", value: Constants.RW.damage}
+            {label: props.intl.formatMessage({id: "subject.sua.rw-shield"}), skill: "R", value: Constants.RW.shield, type: "shield"},
+            {label: props.intl.formatMessage({id: "subject.sua.rw-damage"}), skill: "R", value: Constants.RW.damage}
         ],
         [
             {label: "RE", skill: "R", value: Constants.RE.damage},
-            {label: "RE(栞あり)", skill: "R", value: Constants.RE.bookmark_damage},
-            {label: "RE回復最小値", skill: "R", value: Constants.RE.heal, type: "heal"},
-            {label: "RE回復最大値", skill: "R", value: Constants.RE.heal, type: "heal", multiplier: [{basic: Constants.RE.heal_max_multiplier * 100}]},
+            {label: props.intl.formatMessage({id: "subject.sua.re-bookmark"}), skill: "R", value: Constants.RE.bookmark_damage},
+            {label: props.intl.formatMessage({id: "subject.sua.re-heal-min"}), skill: "R", value: Constants.RE.heal, type: "heal"},
+            {label: props.intl.formatMessage({id: "subject.sua.re-heal-max"}), skill: "R", value: Constants.RE.heal, type: "heal", multiplier: [{basic: Constants.RE.heal_max_multiplier * 100}]},
         ]
     ]   
-}
+})
 
 export default table;
