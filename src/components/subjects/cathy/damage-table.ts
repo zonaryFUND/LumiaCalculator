ed88e5016596e96666c58cc77f625111512843ac
zonaryFUND <zonaryfund@gmail.com>
2024-05-27 21:07:12 +0900

    cathy damage table / 1.22 fix

--- src/components/subjects/cathy/damage-table.ts
+++ src/components/subjects/cathy/damage-table.ts
@@ -1,35 +1,34 @@
 import { WeaponTypeID } from "app-types/equipment/weapon";
-import { DamageTable, SkillValueProps } from "../damage-table";
+import { DamageTable, DamageTableGenerator, SkillValueProps } from "../damage-table";
 import Constants from "./constants.json";
 
-function table(props: {weaponType: WeaponTypeID}): DamageTable {
-
+const table: DamageTableGenerator = props => {
     return {
         basicAttack: [
             "standard",
-            props.weaponType == "dual_swords" ? {label: "Q効果時基本攻撃力", skill: "Q", value: {attack: Constants.Q.dual_sword}, type: "basic"} : undefined,
-            {label: "Q追加ダメージ", skill: "Q", damage: Constants.Q.additional_damage}
+            props.weaponType == "dual_swords" ? {label: props.intl.formatMessage({id: "subject.cathy.q-dualsword-attack"}), skill: "Q", value: {attack: Constants.Q.dual_sword}, type: "basic"} : undefined,
+            {label: props.intl.formatMessage({id: "subject.cathy.q-additional-damage"}), skill: "Q", value: Constants.Q.additional_damage}
         ].filter(v => v) as SkillValueProps[],
         skill: [
             [{label: "Q", skill: "Q", value: Constants.Q.damage}],
             [
-                {label: "W内周", skill: "W", value: Constants.W.inner_damage},
-                {label: "W外周", skill: "W", value: Constants.W.outer_damage}
+                {label: props.intl.formatMessage({id: "subject.cathy.w-inner"}), skill: "W", value: Constants.W.inner_damage},
+                {label: props.intl.formatMessage({id: "subject.cathy.w-outer"}), skill: "W", value: Constants.W.outer_damage}
             ],
             [
                 {label: "E", skill: "E", value: Constants.E.damage},
-                {label: "E気絶時追加ダメージ", skill: "E", value: Constants.E.knockback_damage}
+                {label: props.intl.formatMessage({id: "subject.cathy.e-stun-additional"}), skill: "E", value: Constants.E.knockback_damage}
             ],
             [
-                {label: "R使用・味方蘇生時回復/秒", skill: "R", value: Constants.R.heal, type: "heal"},
-                {label: `R使用・味方蘇生時回復最大(${Constants.R.heal_duration})`, skill: "R", value: Constants.R.heal, type: "heal", multiplier: [{basic: Constants.R.heal_duration * 100}]},
-                {label: "R最小ダメージ", skill: "R", value: Constants.R.min_damage},
-                {label: "R最大ダメージ", skill: "R", value: Constants.R.min_damage, multiplier: [{basic: Constants.R.max_damage_ratio * 100}]}
+                {label: props.intl.formatMessage({id: "subject.cathy.r-heal-per-sec"}), skill: "R", value: Constants.R.heal, type: "heal"},
+                {label: props.intl.formatMessage({id: "subject.cathy.r-heal-max"}, {value: Constants.R.heal_duration}), skill: "R", value: Constants.R.heal, type: "heal", multiplier: [{basic: Constants.R.heal_duration * 100}]},
+                {label: props.intl.formatMessage({id: "subject.cathy.r-min"}), skill: "R", value: Constants.R.min_damage},
+                {label: props.intl.formatMessage({id: "subject.cathy.r-max"}), skill: "R", value: Constants.R.min_damage, multiplier: [{basic: Constants.R.max_damage_ratio * 100}]}
             ],
             [
-                {label: "T外傷合計固定ダメージ", skill: "T", value: Constants.T.wound, type: "true"},
-                {label: "T致命的外傷合計固定ダメージ", skill: "T", value: Constants.T.critical_wound, type: "true"},
-                {label: "Tシールド", skill: "T", value: Constants.T.shield, type: "shield"}
+                {label: props.intl.formatMessage({id: "subject.cathy.passive-wound"}), skill: "T", value: Constants.T.wound, type: "true"},
+                {label: props.intl.formatMessage({id: "subject.cathy.passive-critical-wound"}), skill: "T", value: Constants.T.critical_wound, type: "true"},
+                {label: props.intl.formatMessage({id: "subject.cathy.passive-shield"}), skill: "T", value: Constants.T.shield, type: "shield"}
             ]
         ]
     }
