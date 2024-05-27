6f450740a27f2bbc00a92ec1e2a49b43fd2d0bff
zonaryFUND <zonaryfund@gmail.com>
2024-05-27 22:16:14 +0900

    jenny damage table locale

--- src/components/subjects/jenny/damage-table.ts
+++ src/components/subjects/jenny/damage-table.ts
@@ -1,23 +1,23 @@
-import { DamageTable, SkillValueProps } from "../damage-table";
+import { DamageTable, DamageTableGenerator, SkillValueProps } from "../damage-table";
 import Constants from "./constants.json";
 
-const table: DamageTable = {
+const table: DamageTableGenerator = props => ({
     basicAttack: [
         "standard",
-        {label: "E追加ダメージ", skill: "T", value: Constants.E.damage}
+        {label: props.intl.formatMessage({id: "subject.jenny.e-additional"}), skill: "T", value: Constants.E.damage}
     ],
     skill: [
         [
-            {label: "Q1ヒット", skill: "Q", value: Constants.Q.damage},
-            {label: `Q全ヒット${Constants.Q.count}`, skill: "Q", value: Constants.Q.damage, multiplier: [{basic: Constants.Q.count * 100}]}
+            {label: props.intl.formatMessage({id: "subject.jenny.q-1hit"}), skill: "Q", value: Constants.Q.damage},
+            {label: props.intl.formatMessage({id: "subject.jenny.q-max-hit"}, {value: Constants.Q.count}), skill: "Q", value: Constants.Q.damage, multiplier: [{basic: Constants.Q.count * 100}]}
         ],
         [
             {label: "W1", skill: "W", value: Constants.W.first_damage},
             {label: "W2", skill: "W", value: Constants.W.second_damage}
         ],
         [{label: "R", skill: "R", value: Constants.R.damage}],
-        [{label: "T死の演技復活後回復", skill: "T", value: Constants.T.hp, type: "heal"}]
+        [{label: props.intl.formatMessage({id: "subject.jenny.passive-heal"}), skill: "T", value: Constants.T.hp, type: "heal"}]
     ]   
-}
+})
 
 export default table;
