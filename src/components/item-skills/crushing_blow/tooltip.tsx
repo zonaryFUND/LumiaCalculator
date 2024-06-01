a22c79ded7e021b45a740fe4ed928c561598d25a
zonaryfund <zonaryfund@gmail.com>
2024-06-01 18:36:54 +0900

    1.22 arms

---
+++ src/components/item-skills/crushing_blow/tooltip.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import Constants from "./constants.json";
+import Value from "../value";
+import style from "components/tooltip/tooltip.module.styl";
+
+const description: React.FC = props => {
+    return (
+        <p>
+            戦闘状態になった後、{Constants.cooldown}秒ごとに強化基本攻撃をチャージします。強化基本攻撃は
+            <Value ratio={Constants.damage} overrideExpression={{result: {className: style.level}}} />の追加スキルダメージを与えて
+            <Value ratio={Constants.heal} overrideExpression={{result: {className: style.level}}} />の体力を回復します。
+        </p> 
+    );
+}
+
+export default description;
