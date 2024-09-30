18a8504bc0e524545f2ade522ca06f5505fc10e1
zonaryFUND <zonaryfund@gmail.com>
2024-10-01 02:56:43 +0900

    1.31

--- src/components/subjects/lenore/t.tsx
+++ src/components/subjects/lenore/t.tsx
@@ -15,7 +15,7 @@ const t: React.FC<SubjectSkillProps> = props => {
             <span className={style.emphasis}>アッチェレランド</span>に変換されます。レノアは<span className={style.emphasis}>アッチェレランド</span>
             の数値に応じてクールダウン減少が4%で適用されます。<br />
             <br />
-            <span className={style.emphasis}>苦痛のメロディー：</span>体力が50%より低い実験体にスキルでダメージを与えると、<Value skill="T" ratio={Constants.T.additional_damage} />
+            <span className={style.emphasis}>苦痛のメロディー：</span>体力が50%より低い実験体にスキルでダメージを与えると、<Value skill="T" ratio={Constants.T.additional_damage} overrideExpression={{stack: {format: "悲鳴スタック数"}}} />
             のスキルダメージを追加で与えて悲鳴スタックを獲得します。同じ敵実験体を攻撃する場合、{Constants.T.target_cooldown}秒ごとに効果が適用されます。<br />
             <br />
             <span className={style.emphasis}>トリル：</span>レノアが基本スキルを使用する時、3回目に使用するスキルが強化されます。<br />
