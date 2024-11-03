43f90d31072faad283c2fd720de030e32c9dcdac
zonaryFUND <zonaryfund@gmail.com>
2024-11-04 00:58:18 +0900

    tsubame t tooltip fix

--- src/components/subjects/tsubame/t.tsx
+++ src/components/subjects/tsubame/t.tsx
@@ -7,12 +7,27 @@ import Value from "components/tooltip/value";
 import { ValuesProps } from "../values";
 
 const t: React.FC<SubjectSkillProps> = props => {
+    const { showEquation } = useValueContext();
+    const damage = (() => {
+        if (showEquation) {
+            return <>
+                <span className={style.emphasis}>{Constants.T.damage.base}</span>
+                <span className={style.attack}>(+追加攻撃力の{Constants.T.damage.additionalAttack}%)</span>
+                <span className={style.maxhp}>(+対象の最大体力の{Constants.T.damage.targetMaxHP.base[props.skillLevel]}</span>
+                <span className={style.attack}>(+追加攻撃力の{Constants.T.damage.targetMaxHP.additionalAttack}%)</span>
+                <span className={style.maxhp}>%)</span>
+            </>
+        } else {
+            return <Value skill="T" ratio={Constants.T.damage} />;
+        }
+    })()
+
     return (
         <>
             つばめの基本攻撃は{Constants.T.stack_duration}秒間<span className={style.emphasis}>秘技 - 生死の刻印</span>
             スタックを残します。{Constants.T.max_stack}スタックになると、つばめが攻撃をする時、
             <span className={style.emphasis}>秘技 - 生死の刻印</span>を切り刻んで
-            <Value skill="T" ratio={Constants.T.damage} />のスキルダメージを与えます。
+            {damage}のスキルダメージを与えます。
         </>
     );
 }
