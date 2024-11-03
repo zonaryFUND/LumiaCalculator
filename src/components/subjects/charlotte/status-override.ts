951d694a56d95922363279a65acfa9f381b21b08
zonaryFUND <zonaryfund@gmail.com>
2024-11-04 03:19:33 +0900

    chalotte table / r amp

---
+++ src/components/subjects/charlotte/status-override.ts
@@ -0,0 +1,16 @@
+import Constants from "./constants.json";
+import Decimal from "decimal.js";
+import { StatusOverrideFunc } from "../status-override";
+
+const f: StatusOverrideFunc = (status, config) => ({
+    ...status,
+    skillAmp: {
+        ...status.skillAmp,
+        overrideAdditional: {
+            nameKey: "subject.charlotte.r-amp",
+            value: new Decimal(Constants.R.amp[config.skillLevels.R])
+        }
+    }
+});
+
+export default f;
